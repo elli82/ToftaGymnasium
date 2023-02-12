8f87aff14ca36ee17f4d9a75dcd80c55ba50333b
elli82 <114058163+elli82@users.noreply.github.com>
2023-02-12 16:24:50 +0100

    final edition

---
+++ Labb3.Skola/Menu.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Labb3.Skola.Models;
+using System.Linq;
+
+namespace Labb3.Skola
+{
+    class Menu
+    {
+
+        public static void RunMainMenu()
+        {
+                Console.WriteLine("Tofta Gymnasium Administration Tool");
+                Console.WriteLine();
+                Console.WriteLine("-----------Menu:------------");
+                Console.WriteLine();
+                Console.WriteLine("--------Students------------");               
+                Console.WriteLine("All Students-------------[1]");
+                Console.WriteLine("Info of single Student---[2]");
+                Console.WriteLine();
+                Console.WriteLine("Classes------------------[3]");
+                Console.WriteLine();
+                Console.WriteLine("Courses------------------[4]");
+                Console.WriteLine();
+                Console.WriteLine("---------Staff--------------");
+                Console.WriteLine("Info of Departments------[5]");
+                Console.WriteLine("Add member of the Staff--[6]");
+                Console.WriteLine();
+                Console.WriteLine("Exit---------------------[7]");
+
+            try
+            {
+                int select = int.Parse(Console.ReadLine());
+
+
+                switch (select)
+                {
+                    case 1:
+                        StudentMethod.ListAllStudents();
+                        Console.WriteLine("Press enter for return to main menu.");
+                        break;
+                    case 2:
+                        StudentMethod.SingleStudentInfo();
+                        Console.WriteLine("Press enter for return to main menu.");
+                        break;
+                    case 3:
+                        ClassMethod.ClassLists();
+                        Console.WriteLine("Press enter for return to main menu.");
+                        break;
+                    case 4:
+                        CourseMethod.PrintCourses();
+                        Console.WriteLine("Press enter for return to main menu.");
+                        break;
+                    case 5:
+                        StaffMethod.MembersofStaff();
+                        Console.WriteLine("Press enter for return to main menu.");
+                        break;
+                    case 6:
+                        StaffMethod.AddtoStaff();
+                        Console.WriteLine("Press enter for return to main menu.");
+                        break;
+                    case 7:
+                        Console.WriteLine("Thank you for using Tofta Gymnasium Administration Tool.");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Press enter for main menu.");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+            Console.Clear();
+            
+            
+        }
+    }
+}
---
+++ Labb3.Skola/StudentMethod.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Labb3.Skola.Models;
+
+
+namespace Labb3.Skola
+{
+    class StudentMethod
+    {
+        public static void ListAllStudents()
+        {
+            ToftaGymnasiumDbContext context = new ToftaGymnasiumDbContext();
+            try
+            {
+                Console.WriteLine("For a list of all Students, by Lastnames, press 1:");
+                Console.WriteLine("For a list of all Students, by Firstnames, press 2:");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Console.WriteLine("Ascending, press 1:");
+                    Console.WriteLine("Descending, press 2");
+                    int c = int.Parse(Console.ReadLine());
+                    if (c == 1)
+                    {
+                        var listofstudents = from stud in context.Students
+                                             where stud.StudentId > 0
+                                             orderby stud.LastName ascending
+                                             select stud;
+                        Console.WriteLine("----All Students----");
+                        foreach (var item in listofstudents)
+
+                        {
+                            Console.WriteLine("{0}, {1}, StudentId:{2}", item.LastName, item.FirstName, item.StudentId);
+                        }
+                    }
+                    if (c == 2)
+                    {
+                        var listofstudents = from stud in context.Students
+                                             where stud.StudentId > 0
+                                             orderby stud.LastName descending
+                                             select stud;
+                        Console.WriteLine("----All Students----");
+                        foreach (var item in listofstudents)
+
+                        {
+                            Console.WriteLine("{0}, {1}, StudentId:{2}", item.LastName, item.FirstName, item.StudentId);
+                        }
+
+                    }
+
+                }
+                if (choice == 2)
+                {
+                    Console.WriteLine("Ascending, press 1:");
+                    Console.WriteLine("Desscending, press 2");
+                    int c = int.Parse(Console.ReadLine());
+                    if (c == 1)
+                    {
+                        var listofstudents = from stud in context.Students
+                                             where stud.StudentId > 0
+                                             orderby stud.FirstName ascending
+                                             select stud;
+                        Console.WriteLine("----All Students----");
+                        foreach (var item in listofstudents)
+
+                        {
+                            Console.WriteLine("{0} {1}, StudentId:{2}", item.FirstName, item.LastName, item.StudentId);
+                        }
+                    }
+                    if (c == 2)
+                    {
+                        var listofstudents = from stud in context.Students
+                                             where stud.StudentId > 0
+                                             orderby stud.FirstName descending
+                                             select stud;
+                        Console.WriteLine("----All Students----");
+                        foreach (var item in listofstudents)
+
+                        {
+                            Console.WriteLine("{0} {1}, StudentId:{2}", item.FirstName, item.LastName, item.StudentId);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+            public static void SingleStudentInfo() 
+        {
+            ToftaGymnasiumDbContext context = new ToftaGymnasiumDbContext();
+            try
+            {
+                Console.WriteLine("For info of Student, enter StudentId");
+                int choice = int.Parse(Console.ReadLine());
+
+                var StSingle = from s in context.Students
+                               join cl in context.Classes
+                               on s.ClassId equals cl.ClassId
+                               where s.StudentId == choice
+                               select new
+                               {
+                                   Id = s.StudentId,
+                                   FirstName = s.FirstName,
+                                   LastName = s.LastName,
+                                   PersonalNumber = s.PersonalNr,
+                                   Class = cl.Class,
+                                   Prog = cl.Program
+                               };
+                foreach (var item in StSingle)
+                {
+                    Console.WriteLine("Student with Id nr {0}: {1} {2}", item.Id, item.FirstName, item.LastName);
+                    Console.WriteLine("Personalnumber: {0}", item.PersonalNumber);
+                    Console.WriteLine(" Program: {0}, Class: {1}", item.Prog, item.Class);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
